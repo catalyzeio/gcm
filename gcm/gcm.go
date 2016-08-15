81848a08f3baf5d0a797c7643590c96813ae2dd1
Nathan Sweet <nathanjsweet@gmail.com>
2016-08-15 10:02:20 -0500

    General Cleanup and Refactoring 1. Remove commented code. 2. Add Close function to EncryptFileReader, as well as lock. 3. Remove finalizer stuff. 4. Made EncryptFileReader Read method logic simpler.

--- gcm/gcm.go
+++ gcm/gcm.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"runtime"
 	"sync"
 )
 
@@ -17,9 +16,10 @@ const (
 	AAD = "7f57c07ee9459ed704d5e403086f6503"
 )
 
-// EncryptFile encrypts the file at the specified path using GCM
+// EncryptFile encrypts the file at the specified path using GCM.
 func EncryptFile(inFilePath, outFilePath string, key, givenIV, aad []byte) error {
 	efr, err := NewEncryptFileReader(inFilePath, key, givenIV, aad)
+	defer efr.Close()
 	if err != nil {
 		return err
 	}
@@ -48,55 +48,8 @@ func EncryptFile(inFilePath, outFilePath string, key, givenIV, aad []byte) error
 	return nil
 }
 
-/*func EncryptFile(inFilePath, outFilePath string, key, givenIV, aad []byte) error {
-	if _, err := os.Stat(inFilePath); os.IsNotExist(err) {
-		return fmt.Errorf("A file does not exist at %s", inFilePath)
-	}
-	// copy the IV since it will potentially be incremented
-	iv := make([]byte, len(givenIV))
-	copy(iv, givenIV)
-
-	inFile, err := os.Open(inFilePath)
-	if err != nil {
-		return err
-	}
-	defer inFile.Close()
-	outFile, err := os.OpenFile(outFilePath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0600)
-	if err != nil {
-		return err
-	}
-	defer outFile.Close()
-
-	aes, err := aes.NewCipher(key)
-	if err != nil {
-		return err
-	}
-	gcm, err := cipher.NewGCMWithNonceSize(aes, len(iv))
-	if err != nil {
-		return err
-	}
-
-	chunk := make([]byte, chunkSize)
-	written := false
-	for {
-		read, err := inFile.Read(chunk)
-		// ensure we have written at least one chunk before breaking
-		if read > 0 || !written {
-			encrChunk := gcm.Seal(nil, iv, chunk[:read], aad)
-			if _, err := outFile.Write(encrChunk); err != nil {
-				return err
-			}
-			written = true
-		}
-		if err == io.EOF {
-			break
-		}
-		incrementIV(iv)
-	}
-	return nil
-}*/
-
-// EncryptFileReader is an io.Reader interface that encrypts a file
+// EncryptFileReader is an io.ReadCloser interface that encrypts a file
+// into Read calls.
 type EncryptFileReader struct {
 	file        *os.File
 	gcm         cipher.AEAD
@@ -106,14 +59,23 @@ type EncryptFileReader struct {
 	chunk       []byte
 	doneReading bool
 	written     bool
+	readLock    *sync.Mutex
 }
 
-// Read is the encrypting stream method
+// Read is the encrypting stream method. It reads from its file
+// at a particular chunk size, which may be bigger or smaller than
+// the buffer passed to this Read method. It then decrypts this fixed-size
+// chunk read into a member and copies as much of this saved buffer
+// into the passed argument as it can. If it cannot copy the entire
+// buffer it passes the remainder in the subsequent call(s). If it
+// copies the entirety of the decrypted buffer, the buffer is set to nil
+// and the process is repeated.
 func (efr *EncryptFileReader) Read(p []byte) (int, error) {
-	lenP := len(p)
+	efr.readLock.Lock()
+	defer efr.readLock.Unlock()
 	var read int
 	var err error
-	if efr.leftOver == nil && !efr.doneReading {
+	if !efr.doneReading && len(efr.leftOver) == 0 {
 		read, err = efr.file.Read(efr.chunk)
 		if err != nil && err != io.EOF {
 			return 0, err
@@ -127,37 +89,39 @@ func (efr *EncryptFileReader) Read(p []byte) (int, error) {
 			efr.doneReading = true
 		}
 	}
-	lenLO := len(efr.leftOver)
-	err = nil
-	if lenLO <= lenP {
-		if efr.doneReading {
-			err = io.EOF
-		}
-		copy(p, efr.leftOver)
-		efr.leftOver = nil
-		return lenLO, err
+	copied := copy(p, efr.leftOver)
+	efr.leftOver = efr.leftOver[copied:]
+	if efr.doneReading && len(efr.leftOver) == 0 {
+		return copied, io.EOF
 	}
-	copy(p, efr.leftOver[:lenP])
-	efr.leftOver = efr.leftOver[lenP:]
-	return lenP, nil
+	return copied, nil
 }
 
-func encryptFileReaderFinalizer(efr *EncryptFileReader) {
-	if efr.file != nil {
-		efr.file.Close()
-		efr.file = nil
-	}
+// Close closes the file.
+func (efr *EncryptFileReader) Close() error {
+	efr.readLock.Lock()
+	defer efr.readLock.Unlock()
+	// Close can handle nil calls
+	err := efr.file.Close()
+	efr.file = nil
+	return err
 }
 
-// NewEncryptFileReader creates an instance of EncryptFileReader
+// NewEncryptFileReader creates an instance of EncryptFileReader, which implements io.ReaderCloser,
+// which encrypts its given file in chunks as its Read method is called.
 func NewEncryptFileReader(inFilePath string, key, givenIV, aad []byte) (*EncryptFileReader, error) {
 	efr := new(EncryptFileReader)
 	efr.chunk = make([]byte, chunkSize)
 	efr.doneReading = false
 	efr.written = false
+	efr.readLock = &sync.Mutex{}
 	var err error
 	if _, err = os.Stat(inFilePath); os.IsNotExist(err) {
 		return nil, fmt.Errorf("A file does not exist at %s", inFilePath)
+	}
+	efr.file, err = os.Open(inFilePath)
+	if err != nil {
+		return nil, err
 	}
 	// make our own copy so that it won't get modified
 	efr.aad = make([]byte, len(aad))
@@ -165,12 +129,6 @@ func NewEncryptFileReader(inFilePath string, key, givenIV, aad []byte) (*Encrypt
 	// copy the IV since it will potentially be incremented
 	efr.iv = make([]byte, len(givenIV))
 	copy(efr.iv, givenIV)
-
-	efr.file, err = os.Open(inFilePath)
-	if err != nil {
-		return nil, err
-	}
-	runtime.SetFinalizer(efr, encryptFileReaderFinalizer)
 
 	aes, err := aes.NewCipher(key)
 	if err != nil {
@@ -184,7 +142,7 @@ func NewEncryptFileReader(inFilePath string, key, givenIV, aad []byte) (*Encrypt
 	return efr, nil
 }
 
-// DecryptFile decrypts the file at the specified path using GCM
+// DecryptFile decrypts the file at the specified path using GCM.
 func DecryptFile(inFilePath, outFilePath string, key, givenIV, aad []byte) error {
 	dfw, err := NewDecryptFileWriterAt(outFilePath, key, givenIV, aad)
 	defer dfw.Close()
@@ -220,59 +178,8 @@ func DecryptFile(inFilePath, outFilePath string, key, givenIV, aad []byte) error
 	return nil
 }
 
-/*func DecryptFile(inFilePath, outFilePath string, key, givenIV, aad []byte) error {
-	if _, err := os.Stat(inFilePath); os.IsNotExist(err) {
-		return fmt.Errorf("A file does not exist at %s", inFilePath)
-	}
-	// copy the IV since it will potentially be incremented
-	iv := make([]byte, len(givenIV))
-	copy(iv, givenIV)
-
-	inFile, err := os.Open(inFilePath)
-	if err != nil {
-		return err
-	}
-	defer inFile.Close()
-
-	outFile, err := os.OpenFile(outFilePath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0600)
-	if err != nil {
-		return err
-	}
-	defer outFile.Close()
-
-	aes, err := aes.NewCipher(key)
-	if err != nil {
-		return err
-	}
-	gcm, err := cipher.NewGCMWithNonceSize(aes, len(iv))
-	if err != nil {
-		return err
-	}
-
-	written := false
-	chunk := make([]byte, chunkSize+gcm.Overhead())
-	for {
-		read, err := inFile.Read(chunk)
-		// ensure we have written at least one chunk before breaking
-		if read > 0 || !written {
-			decrChunk, err := gcm.Open(nil, iv, chunk[:read], aad)
-			if err != nil {
-				return err
-			}
-			if _, err := outFile.Write(decrChunk); err != nil {
-				return err
-			}
-			written = true
-		}
-		if err == io.EOF {
-			break
-		}
-		incrementIV(iv)
-	}
-	return nil
-}*/
-
-// DecryptFileWriterAt is an io.WriterAt interface that decrypts a file
+// DecryptFileWriterAt is an io.WriterAt and io.Closer interfaces that decrypts
+// WriterAt calls to a file.
 type DecryptFileWriterAt struct {
 	file        *os.File
 	gcm         cipher.AEAD
@@ -287,12 +194,24 @@ type DecryptFileWriterAt struct {
 	written     bool
 }
 
-// WriteAt is the decrypting stream method
+// WriteAt is the decrypting stream method for DecryptFileWriterAt.
+// It will always the bytes written as the length of the passed buffer. This is a lie.
+// It works by saving byte chunks in a map of offsets as a moving buffer,
+// keeping track of a partial view of the buffer.
+// When it has a complete set of offsets that it can write, without gaps,
+// it copies the offsets to a chunk buffer, a fixed size buffer, required
+// for correct writes to the gcm.Open function. When this fixed size buffer
+// is filled it is decrypted and the resulting decryption is written
+// to the file. When the DecryptFileWriterAt Close function is called,
+// any remaining buffer in the "chunk" buffer is decrypted and written
+// to the file, which is then closed.
 func (dfw *DecryptFileWriterAt) WriteAt(p []byte, off int64) (int, error) {
 	lenP := len(p)
 	dfw.writeLock.Lock()
 	defer dfw.writeLock.Unlock()
 	if off == dfw.nextOff {
+		// This doesn't need to be copied, because we know it's about to get copied to
+		// the chunk buffer.
 		dfw.bufferMap[off] = p
 		for buf, ok := dfw.bufferMap[dfw.nextOff]; ok; buf, ok = dfw.bufferMap[dfw.nextOff] {
 			lenB := len(buf)
@@ -310,36 +229,36 @@ func (dfw *DecryptFileWriterAt) WriteAt(p []byte, off int64) (int, error) {
 	return lenP, nil
 }
 
-// Close flushes any remaining buffer and closes the input file
+// Close flushes any remaining buffer and closes the input file.
 func (dfw *DecryptFileWriterAt) Close() error {
 	dfw.writeLock.Lock()
 	defer dfw.writeLock.Unlock()
 	var err error
+	var err2 error
 	if dfw.chunkCopied > 0 || !dfw.written {
 		err = dfw.flushChunk()
 	}
-	if dfw.file != nil {
-		err2 := dfw.file.Close()
-		dfw.file = nil
-		if err2 != nil {
-			if err == nil {
-				return err2
-			}
-			return fmt.Errorf("%s\n%s", err, err2)
+	// Close can handle nil calls
+	err2 = dfw.file.Close()
+	dfw.file = nil
+	if err2 != nil {
+		if err == nil {
+			return err2
 		}
+		return fmt.Errorf("%s\n%s", err, err2)
 	}
 	return err
 }
 
-func (dfw *DecryptFileWriterAt) copyToChunk(p []byte, pLen int) error {
+func (dfw *DecryptFileWriterAt) copyToChunk(p []byte, lenP int) error {
 	copied := copy(dfw.chunk[dfw.chunkCopied:], p)
 	dfw.chunkCopied += copied
 	if dfw.chunkCopied == dfw.chunkL {
 		dfw.flushChunk()
 	}
-	pLen -= copied
-	if pLen > 0 {
-		return dfw.copyToChunk(p[copied:], pLen)
+	lenP -= copied
+	if lenP > 0 {
+		return dfw.copyToChunk(p[copied:], lenP)
 	}
 	return nil
 }
@@ -358,11 +277,8 @@ func (dfw *DecryptFileWriterAt) flushChunk() error {
 	return nil
 }
 
-func decryptFileWriterAtFinalizer(dfw *DecryptFileWriterAt) {
-	dfw.Close()
-}
-
-// NewDecryptFileWriterAt creates an instance of NewDecryptFileWriterAt
+// NewDecryptFileWriterAt creates an instance of NewDecryptFileWriterAt, which decrypts
+// WrittenAt chunks into the given file path.
 func NewDecryptFileWriterAt(outFilePath string, key, givenIV, aad []byte) (*DecryptFileWriterAt, error) {
 	dfw := new(DecryptFileWriterAt)
 	dfw.nextOff = 0
@@ -374,7 +290,6 @@ func NewDecryptFileWriterAt(outFilePath string, key, givenIV, aad []byte) (*Decr
 	if err != nil {
 		return nil, err
 	}
-	runtime.SetFinalizer(dfw, decryptFileWriterAtFinalizer)
 	// make our own copy so that it won't get modified
 	dfw.aad = make([]byte, len(aad))
 	copy(dfw.aad, aad)
