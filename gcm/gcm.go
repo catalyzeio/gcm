a2ba77883ea18442a83a31ae4c4a4597f8299d06
Adam Leko <adam@catalyze.io>
2016-08-29 22:31:25 -0500

    First pass at addressing the short reads bug

--- gcm/gcm.go
+++ gcm/gcm.go
@@ -3,248 +3,225 @@ package gcm
 import (
 	"crypto/aes"
 	"crypto/cipher"
+	"fmt"
 	"io"
 	"os"
 )
 
 const (
-	chunkSize = 1024 * 1024 // 1 MB
+	chunkSize = 1024 * 1024 // 1 MiB
 
 	// AAD (Additional authenticated data) is to be used in the GCM algorithm
 	AAD = "7f57c07ee9459ed704d5e403086f6503"
 )
 
 // EncryptFile encrypts the file at the specified path using GCM.
-func EncryptFile(inFilePath, outFilePath string, key, givenIV, aad []byte) error {
+func EncryptFile(inFilePath, outFilePath string, key, iv, aad []byte) error {
 	if _, err := os.Stat(inFilePath); os.IsNotExist(err) {
-		return err
-	}
-	file, err := os.Open(inFilePath)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-	efr, err := NewEncryptReader(file, key, givenIV, aad)
-	if err != nil {
-		return err
-	}
-	outFile, err := os.OpenFile(outFilePath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0600)
-	if err != nil {
-		return err
-	}
-	defer outFile.Close()
-	chunk := make([]byte, chunkSize)
-	written := false
-	for {
-		read, err := efr.Read(chunk)
-		if err != nil && err != io.EOF {
-			return err
-		}
-		if read > 0 || !written {
-			if _, err := outFile.Write(chunk[:read]); err != nil {
-				return err
-			}
-			written = true
-		}
-		if err == io.EOF {
-			break
-		}
-	}
-	return nil
-}
-
-// EncryptReader is an io.Reader interface that encrypts its
-// read calls into another io.Reader interface
-type EncryptReader struct {
-	reader      io.Reader
-	gcm         cipher.AEAD
-	aad         []byte
-	iv          []byte
-	leftOver    []byte
-	chunk       []byte
-	doneReading bool
-	written     bool
-}
-
-// Read is the encrypting stream method. It reads from its io.Reader
-// at a particular chunk size, which may be bigger or smaller than
-// the buffer passed to this Read method. It then decrypts this fixed-size
-// chunk read into a member (leftOver) and copies as much of this saved buffer
-// into the passed buffer-argument as it can. If it cannot copy the entire
-// "leftOver" buffer it passes the remainder in the subsequent call(s). When
-// "leftOver" buffer reaches 0 a chunk read happens again, unitl the reader
-// returns an io.EOF. This Read method will return its io.EOF when it has
-// received an io.EOF from its file AND the "leftOver" buffer reaches 0.
-func (efr *EncryptReader) Read(p []byte) (int, error) {
-	if !efr.doneReading && len(efr.leftOver) == 0 {
-		read, err := efr.reader.Read(efr.chunk)
-		if err != nil && err != io.EOF {
-			return 0, err
-		}
-		if read > 0 || !efr.written {
-			efr.leftOver = efr.gcm.Seal(nil, efr.iv, efr.chunk[:read], efr.aad)
-			incrementIV(efr.iv)
-			efr.written = true
-		}
-		if err == io.EOF {
-			efr.doneReading = true
-		}
-	}
-	copied := copy(p, efr.leftOver)
-	efr.leftOver = efr.leftOver[copied:]
-	if efr.doneReading && len(efr.leftOver) == 0 {
-		return copied, io.EOF
-	}
-	return copied, nil
-}
-
-// NewEncryptReader creates an instance of EncryptReader, which implements io.ReaderCloser,
-// which encrypts its given io.Reader in chunks as its Read method is called.
-func NewEncryptReader(reader io.Reader, key, givenIV, aad []byte) (*EncryptReader, error) {
-	efr := new(EncryptReader)
-	efr.reader = reader
-	efr.chunk = make([]byte, chunkSize)
-	efr.doneReading = false
-	efr.written = false
-	// make our own copy so that it won't get modified
-	efr.aad = make([]byte, len(aad))
-	copy(efr.aad, aad)
-	// copy the IV since it will potentially be incremented
-	efr.iv = make([]byte, len(givenIV))
-	copy(efr.iv, givenIV)
-
-	aes, err := aes.NewCipher(key)
-	if err != nil {
-		return nil, err
+		return fmt.Errorf("A file does not exist at %s", inFilePath)
 	}
 
-	efr.gcm, err = cipher.NewGCMWithNonceSize(aes, len(efr.iv))
-	if err != nil {
-		return nil, err
-	}
-	return efr, nil
-}
-
-// DecryptFile decrypts the file at the specified path using GCM.
-func DecryptFile(inFilePath, outFilePath string, key, givenIV, aad []byte) error {
-	outFile, err := os.OpenFile(outFilePath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0600)
-	if err != nil {
-		return err
-	}
-	dfw, err := NewDecryptWriteCloser(outFile, key, givenIV, aad)
-	if err != nil {
-		return err
-	}
 	inFile, err := os.Open(inFilePath)
 	if err != nil {
 		return err
 	}
 	defer inFile.Close()
-	written := false
-	chunk := make([]byte, chunkSize)
-	for {
-		read, err := inFile.Read(chunk)
-		if err != nil && err != io.EOF {
-			return err
-		}
-		// ensure we have written at least one chunk before breaking
-		if read > 0 || !written {
-			_, err := dfw.Write(chunk[:read])
-			if err != nil {
-				return err
-			}
-			written = true
-		}
-		if err == io.EOF {
-			break
-		}
-	}
-	return dfw.Close()
-}
 
-// DecryptWriteCloser is an io.WriteCloser interface that decrypts
-// it Write calls into another io.WriteCloser interface
-type DecryptWriteCloser struct {
-	writeCloser io.WriteCloser
-	written     bool
-	gcm         cipher.AEAD
-	aad         []byte
-	iv          []byte
-	chunk       []byte
-	chunkL      int
-	chunkCopied int
-}
-
-// Write copies the passed write buffer into a fixed chunk size
-// that GCM accepts. The bytes are only flushed to the underlying
-// io.WriteCloser when the chunk buffer reaches this size OR on
-// the last Write call (i.e. the remaining, last bytes).
-func (dfw *DecryptWriteCloser) Write(p []byte) (int, error) {
-	copied := copy(dfw.chunk[dfw.chunkCopied:], p)
-	dfw.chunkCopied += copied
-	if dfw.chunkCopied == dfw.chunkL {
-		if err := dfw.flushChunk(); err != nil {
-			return copied, err
-		}
-	}
-	pRemainder := p[copied:]
-	if len(pRemainder) > 0 {
-		tmp, err := dfw.Write(pRemainder)
-		return tmp + copied, err
-	}
-	return copied, nil
-}
-
-// Close flushes any remaining bytes that have not been written to
-// the underlying io.WriteCloser, and closes the underlying io.WriteCloser
-func (dfw *DecryptWriteCloser) Close() error {
-	if err := dfw.flushChunk(); err != nil {
+	outFile, err := os.OpenFile(outFilePath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0600)
+	if err != nil {
 		return err
 	}
-	return dfw.writeCloser.Close()
-}
+	defer outFile.Close()
 
-func (dfw *DecryptWriteCloser) flushChunk() error {
-	if dfw.chunkCopied > 0 || !dfw.written {
-		decrChunk, err := dfw.gcm.Open(nil, dfw.iv, dfw.chunk[:dfw.chunkCopied], dfw.aad)
-		if err != nil {
-			return err
-		}
-		if _, err := dfw.writeCloser.Write(decrChunk); err != nil {
-			return err
-		}
-		incrementIV(dfw.iv)
-		dfw.chunkCopied = 0
-		dfw.written = true
+	r, err := NewReader(inFile, key, iv, aad)
+	if err != nil {
+		return err
 	}
-	return nil
+
+	_, err = io.Copy(outFile, r)
+	return err
 }
 
-// NewDecryptWriteCloser creates an instance of DecryptWriteCloser, which decrypts
-// io.Writer chunks into the given io.Writer. The type is a Closer, because it must be closed to
-// Write all data its been given (i.e. told when the "last" write is happening).
-func NewDecryptWriteCloser(writeCloser io.WriteCloser, key, givenIV, aad []byte) (*DecryptWriteCloser, error) {
-	dfw := new(DecryptWriteCloser)
-	dfw.writeCloser = writeCloser
-	dfw.written = false
-	// make our own copy so that it won't get modified
-	dfw.aad = make([]byte, len(aad))
-	copy(dfw.aad, aad)
-	// copy the IV since it will potentially be incremented
-	dfw.iv = make([]byte, len(givenIV))
-	copy(dfw.iv, givenIV)
+// DecryptFile decrypts the file at the specified path using GCM.
+func DecryptFile(inFilePath, outFilePath string, key, iv, aad []byte) error {
+	if _, err := os.Stat(inFilePath); os.IsNotExist(err) {
+		return fmt.Errorf("A file does not exist at %s", inFilePath)
+	}
+
+	inFile, err := os.Open(inFilePath)
+	if err != nil {
+		return err
+	}
+	defer inFile.Close()
+
+	outFile, err := os.OpenFile(outFilePath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0600)
+	if err != nil {
+		return err
+	}
+	defer outFile.Close()
+
+	w, err := NewWriter(outFile, key, iv, aad)
+	if err != nil {
+		return err
+	}
+	defer w.Close()
+
+	_, err = io.Copy(w, inFile)
+	return err
+}
+
+// Wraps data from an io.Reader in an encrypted GCM data stream.
+type Reader struct {
+	src io.Reader
+	eof bool
+
+	gcm cipher.AEAD
+	iv  []byte
+	aad []byte
+
+	buff []byte
+	off  int
+}
+
+func NewReader(src io.Reader, key, iv, aad []byte) (*Reader, error) {
+	// copy the IV since it will be incremented
+	ivCopy := make([]byte, len(iv))
+	copy(ivCopy, iv)
 
 	aes, err := aes.NewCipher(key)
 	if err != nil {
 		return nil, err
 	}
-	dfw.gcm, err = cipher.NewGCMWithNonceSize(aes, len(dfw.iv))
+	gcm, err := cipher.NewGCMWithNonceSize(aes, len(iv))
 	if err != nil {
 		return nil, err
 	}
-	dfw.chunkL = chunkSize + dfw.gcm.Overhead()
-	dfw.chunk = make([]byte, dfw.chunkL)
-	return dfw, nil
+
+	return &Reader{
+		src: src,
+
+		gcm: gcm,
+		iv:  ivCopy,
+		aad: aad,
+
+		buff: make([]byte, 0, chunkSize+gcm.Overhead()),
+	}, nil
+}
+
+func (r *Reader) Read(p []byte) (int, error) {
+	n := len(p)
+	off := 0
+	for off < n {
+		// encrypt the next chunk if no data available
+		if r.off >= len(r.buff) {
+			if r.eof {
+				return off, io.EOF
+			}
+			if err := r.fill(); err != nil {
+				return off, err
+			}
+		}
+		// copy encrypted data from the current chunk
+		read := copy(p[off:], r.buff[r.off:])
+		r.off += read
+		off += read
+	}
+	return off, nil
+}
+
+func (r *Reader) fill() error {
+	// pull in the next chunk from the reader
+	n, err := io.ReadFull(r.src, r.buff[:chunkSize])
+	if err == io.EOF || err == io.ErrUnexpectedEOF {
+		// mark EOF reached for subsequent read attempts
+		r.eof = true
+	} else if err != nil {
+		// some other problem; abort
+		return err
+	}
+	// encrypt this chunk
+	r.buff = r.gcm.Seal(r.buff[:0], r.iv, r.buff[:n], r.aad)
+	incrementIV(r.iv)
+	r.off = 0
+	return nil
+}
+
+// Unwraps an encrypted GCM data to the given io.Writer stream.
+type Writer struct {
+	dst io.WriteCloser
+
+	gcm cipher.AEAD
+	iv  []byte
+	aad []byte
+
+	buff []byte
+	off  int
+}
+
+func NewWriter(dst io.WriteCloser, key, iv, aad []byte) (*Writer, error) {
+	// copy the IV since it will be incremented
+	ivCopy := make([]byte, len(iv))
+	copy(ivCopy, iv)
+
+	aes, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	gcm, err := cipher.NewGCMWithNonceSize(aes, len(iv))
+	if err != nil {
+		return nil, err
+	}
+
+	return &Writer{
+		dst: dst,
+
+		gcm: gcm,
+		iv:  ivCopy,
+		aad: aad,
+
+		buff: make([]byte, chunkSize+gcm.Overhead()),
+	}, nil
+}
+
+func (w *Writer) Write(p []byte) (int, error) {
+	n := len(p)
+	off := 0
+	for off < n {
+		// copy encrypted data into the current chunk
+		written := copy(w.buff[w.off:], p[off:])
+		w.off += written
+		off += written
+		// decrypt if chunk is finished
+		if w.off == cap(w.buff) {
+			if err := w.drain(); err != nil {
+				return off, err
+			}
+		}
+	}
+	return off, nil
+}
+
+func (w *Writer) Close() error {
+	if w.off > 0 {
+		if err := w.drain(); err != nil {
+			return err
+		}
+	}
+	return w.dst.Close()
+}
+
+func (w *Writer) drain() error {
+	buff := w.buff
+	buff, err := w.gcm.Open(buff[:0], w.iv, buff[:w.off], w.aad)
+	if err != nil {
+		return err
+	}
+	if _, err := w.dst.Write(buff); err != nil {
+		return err
+	}
+	incrementIV(w.iv)
+	w.off = 0
+	return nil
 }
 
 func incrementIV(iv []byte) {
